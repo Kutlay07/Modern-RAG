7a8b40260c946a937dd98df6d74a03693a854bbd
Kutlay07 <kutlay-mahmut07@hotmail.com>
2026-07-24 17:05:24 +0300

    feat: implement document ingestion pipeline

---
+++ src/config.py
@@ -0,0 +1,9 @@
+EMBEDDING_MODEL = "BAAI/bge-small-en-v1.5"
+
+LLM_MODEL = "google/gemma-3-1b-it"
+
+DOCUMENT_PATH = "data/documents"
+
+VECTOR_DB = "data/vector_db"
+
+TOP_K = 5
---
+++ src/ingest.py
@@ -0,0 +1,36 @@
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import TextLoader
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+import src.config as config
+
+
+def ingest():
+    loader = DirectoryLoader(
+        config.DOCUMENTS_PATH,
+        glob="**/*.txt",
+        loader_cls=TextLoader,
+    )
+
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100,
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name=config.EMBEDDING_MODEL,
+    )
+
+    vector_store = FAISS.from_documents(
+        chunks,
+        embeddings,
+    )
+
+    return vector_store
